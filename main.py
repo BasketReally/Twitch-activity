51bfd1bdf24df734a6d80d3ee056c50cabb7de73
BasketReally <96622297+BasketReally@users.noreply.github.com>
2022-04-02 23:18:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import requests
 import discord
 from discord.ext import tasks, commands
+import time
 
 while True:
   url = "https://api.twitch.tv/helix/streams?user_login=pwgood"
 
   req = requests.get(url, headers = {'Authorization' : 'Bearer vl7w8v42mf0p10dphf7uv0xwsc81tp' , 'Client-Id' :  'gp762nuuoqcoxypju8c569th9wz7q5'}, timeout = 15)
   req.raise_for_status()
-  info = str(req.json())
-  type = info.find("live")
-  if type > 0:
+  info = req.json()
+  data = str(info.get('data'))
+
+  game = data[data.find('game_name')+13 :       data.find('type')-4]
+
+  live = data[data.find('type')+8 : data.find('title')-4]
+
+  name = data[data.find('title')+9 : data.find("viewer_count")-4]
+
+  if live == "live":
+    print(live)
+    print("activity")
     TOKEN ="Njk4MTM1NzY3NzUwNTQxNDAy.Ycl5sA.kHqhaE9qW9ovTuXvZKZGdLSD0M0"
-    client = commands.Bot(
-      command_prefix='+',
-      self_bot=True
-    )
+    client = commands.Bot(command_prefix='+',self_bot=True)
     @client.event
     async def on_connect():
-      await client.change_presence(activity=discord.Streaming(name = "Basket client coding", url = "https://www.twitch.tv/pwgood"))
+      await client.change_presence(activity=discord.Streaming(name = name,game = game, url = "https://www.twitch.tv/pwgood"))
+    print("pre stream")
+    print("stream")
     client.run((TOKEN), bot=False)
+  if live == "":
+    print(live)
+    print("activity  sleep")
+    TOKEN ="Njk4MTM1NzY3NzUwNTQxNDAy.Ycl5sA.kHqhaE9qW9ovTuXvZKZGdLSD0M0"
+    client = commands.Bot(command_prefix='+',self_bot=True)
+    @client.event
+    async def on_connect():
+      await client.change_presence(activity=discord.Streaming(name = "Sleeping", url = "https://www.twitch.tv/pwgood"))
+    print("pre stream")
+    print("non stream")
+    client.run((TOKEN), bot=False)
+  time.sleep(15)
