fe91a691ad72527f93a0313b3795a1bfd18f0def
BasketReally <96622297+BasketReally@users.noreply.github.com>
2022-04-03 22:37:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from discord.ext import tasks, commands
 import time
 
 while True:
-  url = "https://api.twitch.tv/helix/streams?user_login=pwgood"
+
+  name = "pwgood"
+  url = "https://api.twitch.tv/helix/streams?user_login=" + name
 
   req = requests.get(url, headers = {'Authorization' : 'Bearer vl7w8v42mf0p10dphf7uv0xwsc81tp' , 'Client-Id' :  'gp762nuuoqcoxypju8c569th9wz7q5'}, timeout = 15)
+  
   req.raise_for_status()
+  
   info = req.json()
+  
   data = str(info.get('data'))
+  
 
   game = data[data.find('game_name')+13 :       data.find('type')-4]
 
@@ -17,27 +23,33 @@ while True:
 
   name = data[data.find('title')+9 : data.find("viewer_count")-4]
 
+
+  print("game = "+ game)
+  print("type = " + live)
+  print("name = " + name)
+
   if live == "live":
-    print(live)
-    print("activity")
-    TOKEN ="Njk4MTM1NzY3NzUwNTQxNDAy.Ycl5sA.kHqhaE9qW9ovTuXvZKZGdLSD0M0"
+    print("Open the stream status")
+    print(name)
     client = commands.Bot(command_prefix='+',self_bot=True)
+
     @client.event
     async def on_connect():
+
       await client.change_presence(activity=discord.Streaming(name = name,game = game, url = "https://www.twitch.tv/pwgood"))
-    print("pre stream")
-    print("stream")
-    client.run((TOKEN), bot=False)
+
+    client.run("Njk4MTM1NzY3NzUwNTQxNDAy.Ycl5sA.kHqhaE9qW9ovTuXvZKZGdLSD0M0", bot=False)
+    
   if live == "":
-    print(live)
-    print("activity  sleep")
-    TOKEN ="Njk4MTM1NzY3NzUwNTQxNDAy.Ycl5sA.kHqhaE9qW9ovTuXvZKZGdLSD0M0"
     client = commands.Bot(command_prefix='+',self_bot=True)
+
     @client.event
     async def on_connect():
-      await client.change_presence(activity=discord.Streaming(name = "Sleeping", url = "https://www.twitch.tv/pwgood"))
-    print("pre stream")
-    print("non stream")
-    client.run((TOKEN), bot=False)
-    time.sleep(15)
-    print("version 0.1")
+
+      await client.change_presence(activity=discord.Streaming(name = name, url = "https://www.twitch.tv/pwgood"))
+
+    client.run("Njk4MTM1NzY3NzUwNTQxNDAy.Ycl5sA.kHqhaE9qW9ovTuXvZKZGdLSD0M0", bot=False)
+  print("Closing the stream status")
+
+  time.sleep(15)
+  print("version 0.1")
