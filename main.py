d83e1fcc813d90b75f9615d17fecb1d157951bfa
BasketReally <96622297+BasketReally@users.noreply.github.com>
2022-04-17 13:33:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,65 @@
-import requests
-import discord
+#python script for streaming status
+#Basket
+#twitch.api,discord.py,requests
+
+#modules
+import requests, discord
 from discord.ext import tasks, commands
-import time
 
-while True:
+client = commands.Bot(command_prefix='+',self_bot=True) #a client variable
 
-  name = "pwgood"
-  url = "https://api.twitch.tv/helix/streams?user_login=" + name
+global debug
+debug = input("Enable debug mode?(Yes/No)? : ")
 
-  req = requests.get(url, headers = {'Authorization' : 'Bearer vl7w8v42mf0p10dphf7uv0xwsc81tp' , 'Client-Id' :  'gp762nuuoqcoxypju8c569th9wz7q5'}, timeout = 15)
   
-  req.raise_for_status()
+@client.event
+async def on_connect():
+  while True:
+    #main funcion
+    name = "pwgood" #a name variable. Type a twitch user here
+    url = "https://api.twitch.tv/helix/streams?user_login=" + name
+
+    headers = {
+      'Authorization' : 'Bearer vl7w8v42mf0p10dphf7uv0xwsc81tp', 
+      'Client-Id' :  'gp762nuuoqcoxypju8c569th9wz7q5'
+    }
+
+    req = requests.get(url, headers = headers, timeout = 15)#get request to twitch api
   
-  info = req.json()
+    req.raise_for_status()
+    info = req.json()
+    data = str(info.get('data'))
   
-  data = str(info.get('data'))
-  
 
-  game = data[data.find('game_name')+13 :       data.find('type')-4]
+    game = data[data.find('game_name')+13 :       data.find('type')-4]
 
-  live = data[data.find('type')+8 : data.find('title')-4]
+    live = data[data.find('type')+8 : data.find('title')-4]
 
-  viewer_count = str(data[data.find('viewer_count')+17 : data.find("started_at")-4])
+    viewer_count = str(data[data.find('viewer_count')+15 : data.find("started_at")-4])
 
-  name = data[data.find('title')+9 : data.find("viewer_count")-4] + "   Viewers : " + viewer_count
+    title = data[data.find('title')+9 : data.find("viewer_count")-4] + "   Viewers : " + viewer_count
 
-  print("game = "+ game)
-  print("type = " + live)
-  print("name = " + name)
-
-  if live == "live":
-    print("Open the stream status")
-    print(name)
-    client = commands.Bot(command_prefix='+',self_bot=True)
-
-    @client.event
-    async def on_connect():
-
-      await client.change_presence(activity=discord.Streaming(name = name,game = game, url = "https://www.twitch.tv/pwgood"))
-
-    client.run("Njk4MTM1NzY3NzUwNTQxNDAy.Ycl5sA.kHqhaE9qW9ovTuXvZKZGdLSD0M0", bot=False)
+    if debug == "Yes":
+      #debug mode
+      print("data : " + data)
+      print("game = " + game)
+      print("type = " + live)
+      print("name = " + title)
     
-  if live == "":
-    client = commands.Bot(command_prefix='+',self_bot=True)
+    if live == "live":
+      #live mode
+      activity=discord.Streaming(name = title,game = game, url = "https://www.twitch.tv/pwgood")
+      
+    
+    if live == "":
+      #ofline mode
+      activity=discord.Streaming(name = "Sleeping...", url = "https://www.twitch.tv/pwgood")
 
-    @client.event
-    async def on_connect():
+    await client.change_presence(activity=activity) #stream activity
 
-      await client.change_presence(activity=discord.Streaming(name = "Sleeping...", url = "https://www.twitch.tv/pwgood"))
+print("version 0.5")
 
-    client.run("Njk4MTM1NzY3NzUwNTQxNDAy.Ycl5sA.kHqhaE9qW9ovTuXvZKZGdLSD0M0", bot=False)
-  print("Closing the stream status")
 
-  time.sleep(15)
-  print("version 0.3")
+client.run("NjMyMTc2MDQ2NzExNjM1OTY4.Ylswng.zLL5HzG5gxFjbsJF0OX-vopBDAM", bot=False)
+
+
